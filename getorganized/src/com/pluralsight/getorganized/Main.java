2deba47af5ae43a91c7bfaa9157cc11f56b90680
jakob <jakob.lumpkin@ibm.com>
2021-06-27 17:12:37 -0500

    after data type variable

---
+++ getorganized/src/com/pluralsight/getorganized/Main.java
@@ -0,0 +1,9 @@
+package com.pluralsight.getorganized;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //'java com.pluralsight.getorganized.Main': to run in command line
+	    System.out.println("Get organized");
+    }
+}
