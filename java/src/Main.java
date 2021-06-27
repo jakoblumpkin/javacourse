b0460eee4977d54f37fcf294e8c88ff8e0603d9a
jakob <jakob.lumpkin@ibm.com>
2021-06-27 16:18:29 -0500

    added comments

---
+++ java/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+           //How to print out
+           /*How to print things out*/
+	       System.out.println("Hello World Again");
+	       System.out.println("Hello World Again");
+           System.out.println("Hello World Again");
+    }
+}
