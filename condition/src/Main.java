9fa442e065c5c29136607bf9fb75d1aaf74d9b95
jakob <jakob.lumpkin@ibm.com>
2021-06-27 20:05:26 -0500

    conditions

--- condition/src/Main.java
+++ condition/src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int v1 = 7;
-        int v2 = 7;
+        int v1 = 7, v2 = 7;
 
         int result = v1 > v2 ? v1 : v2;
         System.out.print(result);
@@ -14,5 +13,16 @@ public class Main {
         } else {
             System.out.print("\n v1 and v2 are equal.");
         }
+
+        /* Operators
+        *
+        * And &
+        * Or |
+        * Exclusive or (XOR) ^
+        * Negation ! false
+        * Or ||
+        * And && reads left and if false doesn't read right*/
+
+      
     }
 }
